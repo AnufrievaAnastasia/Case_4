ca8b6c49b9da42151f8dd8502143b870a89734ed
AnufrievaAnastasia <anufrieva_01@mail.ru>
2019-11-16 16:13:54 +0700

    2

--- Case_4.py
+++ Case_4.py
@@ -1,16 +1,17 @@
-"""Case-study #4 Анализ текста
-Разработчики:
-Ануфриева А.
-Журавлева А.
+"""Case-study #4 Text analysis
+Developers:
+Anufrieva A.
+Zhuravleva A.
 """
+import local as lc
 
-text = input("Введите текст:").lower()
+text = input(lc.TXT_TEXT).lower()
 
 count_sentens = text.count('. ') + text.count('! ') + text.count('? ') + text.count('?! ') + text.count('... ')
-print('Предложений:', count_sentens)
+print(lc.TXT_SENTENCES, count_sentens)
 
 count_words = len(text.split())
-print('Слов:', count_words)
+print(lc.TXT_WORDS, count_words)
 
 vowel = ['a', 'e', 'i', 'o', 'u', 'у', 'е', 'ы', 'а', 'о', 'э', 'ё', 'я', 'и', 'ю']
 
@@ -18,10 +19,10 @@ count_syllables = 0
 for letter in text:
     if letter in vowel:
         count_syllables += 1
-print('Слогов:', count_syllables)
+print(lc.TXT_SYLLABLE, count_syllables)
 
 ASL = count_words / count_sentens
-print('Средняя длина предложения в словах:', ASL)
+print(lc.TXT_ASL, ASL)
 
 ASW = count_syllables / count_words
-print('Средняя длина слова в слогах:', ASW)
+print(lc.TXT_ASW, ASW)
