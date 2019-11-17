79d1478819c94670a21c42e020391858e0f8e32e
aleksandrazh20 <hollandjoe20@gmail.com>
2019-11-17 22:11:41 +0700

    1

--- Case_4.py
+++ Case_4.py
@@ -3,11 +3,17 @@ Developers:
 Anufrieva A.
 Zhuravleva A.
 """
+from textblob import TextBlob, Word, Blobber
+from textblob.classifiers import NaiveBayesClassifier
+from textblob.taggers import NLTKTagger
+from dostoevsky.tokenization import RegexTokenizer
+from dostoevsky.models import FastTextSocialNetworkModel
+
 import local as lc
 
 text = input(lc.TXT_TEXT).lower()
 
-count_sentens = text.count('. ') + text.count('! ') + text.count('? ') + text.count('?! ') + text.count('... ')
+count_sentens = text.count('.') + text.count('!') + text.count('?') + text.count('?!') + text.count('...')
 print(lc.TXT_SENTENCES, count_sentens)
 
 count_words = len(text.split())
@@ -26,3 +32,22 @@ print(lc.TXT_ASL, ASL)
 
 ASW = count_syllables / count_words
 print(lc.TXT_ASW, ASW)
+
+FRE_RU = 206.835 - (1.3 * ASL) -(60.1 * ASW)
+FRE_EN = 206.835 - (1.015 * ASL) - (84.6 * ASW)
+
+text = TextBlob(text)
+if text.detect_language() == 'en':
+    pol = text.sentiment.polarity
+    sub = text.sentiment.subjectivity
+    print('Индекс удобочитаемости Флеша: ', FRE_EN)
+    if 90 <= FRE_EN <= 100:
+        print('Текст очень легко читать')
+    elif 65 <= FRE_EN < 90:
+        print('Текст не сложный')
+    elif 30 <= FRE_EN < 65:
+        print('Текст немного трудно читать')
+    else:
+        print('Текст очень трудно читать')
+    print('Обьективность: ', (1 - sub) * 100, '%')
+    print('Тональность:', pol)
